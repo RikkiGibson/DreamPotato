a6985af308c4b0a3905e5148dbbca31522710d5f
Rikki Gibson <rigibson@microsoft.com>
2025-09-28 19:13:29 -0700

    Fix opening a file with command line arg

--- src/DreamPotato.MonoGame/Program.cs
+++ src/DreamPotato.MonoGame/Program.cs
@@ -2,12 +2,12 @@
 using System;
 
 Console.WriteLine($"DreamPotato VMU Emulator");
-if (args.Length > 2)
+if (args.Length > 1)
 {
     Console.Error.WriteLine($"Usage: '{args[0]} [vmu-or-vms-path]'");
 }
 
-string? gameFilePath = args is [_, var path] ? path : null;
+string? gameFilePath = args is [var path] ? path : null;
 
 using var game = new DreamPotato.MonoGame.Game1(gameFilePath);
 game.Run();
