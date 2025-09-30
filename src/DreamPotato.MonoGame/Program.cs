9e427210cf1fc81790fa7601bbb29e86147afaab
Rikki Gibson <rigibson@microsoft.com>
2025-09-29 19:45:55 -0700

    cleanup

--- src/DreamPotato.MonoGame/Program.cs
+++ src/DreamPotato.MonoGame/Program.cs
@@ -4,10 +4,12 @@ using System;
 Console.WriteLine($"DreamPotato VMU Emulator");
 if (args.Length > 1)
 {
-    Console.Error.WriteLine($"Usage: '{args[0]} [vmu-or-vms-path]'");
+    Console.Error.WriteLine($"Usage: 'DreamPotato [vmu-or-vms-path]'");
+    return 1;
 }
 
 string? gameFilePath = args is [var path] ? path : null;
 
 using var game = new DreamPotato.MonoGame.Game1(gameFilePath);
 game.Run();
+return 0;
