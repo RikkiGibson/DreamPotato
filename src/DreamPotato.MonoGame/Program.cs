cf093805c675ea8484529b5082eb6e209427dd63
Rikki Gibson <rigibson@microsoft.com>
2025-09-29 19:46:16 -0700

    WIP

--- src/DreamPotato.MonoGame/Program.cs
+++ src/DreamPotato.MonoGame/Program.cs
@@ -1,5 +1,9 @@
 ﻿
 using System;
+using System.Runtime.InteropServices;
+
+if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+    AttachConsole(-1);
 
 Console.WriteLine($"DreamPotato VMU Emulator");
 if (args.Length > 1)
@@ -13,3 +17,10 @@ string? gameFilePath = args is [var path] ? path : null;
 using var game = new DreamPotato.MonoGame.Game1(gameFilePath);
 game.Run();
 return 0;
+
+partial class Program
+{
+    [LibraryImport("kernel32", SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    private static partial bool AttachConsole(int dwProcessId);
+}
