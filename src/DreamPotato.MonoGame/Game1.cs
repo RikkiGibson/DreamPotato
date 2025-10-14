2e35bb51ca7ea5bafbdd70e77e7c792bca56174a
Rikki Gibson <rikkigibson@gmail.com>
2025-10-13 22:01:18 -0700

    Only open a file automatically if it exists

--- src/DreamPotato.MonoGame/Game1.cs
+++ src/DreamPotato.MonoGame/Game1.cs
@@ -140,7 +140,7 @@ public class Game1 : Game
         }
 
         vmsOrVmuFilePath ??= recentFilesInfo?.RecentFiles.FirstOrDefault();
-        if (vmsOrVmuFilePath != null)
+        if (vmsOrVmuFilePath != null && File.Exists(vmsOrVmuFilePath))
         {
             LoadAndStartVmsOrVmuFile(vmu, vmsOrVmuFilePath);
         }
