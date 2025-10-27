913d3d54cfb4f86fe6eaecc6ff482207e72182d8
Rikki Gibson <rikkigibson@gmail.com>
2025-10-26 19:56:42 -0700

    Fix mac build

--- pack-mac-app.sh
+++ pack-mac-app.sh
@@ -2,12 +2,15 @@
 # See https://docs.monogame.net/articles/tutorials/building_2d_games/25_packaging_game/index.html?tabs=macOS#platform-specific-packaging
 scriptroot="$(dirname $0)"
 
+echo "Creating app bundle at '$scriptroot/artifacts/publish/DreamPotato.MonoGame/DreamPotato.app'..."
 mkdir -p $scriptroot/artifacts/publish/DreamPotato.MonoGame/DreamPotato.app/Contents/MacOS/
 mkdir -p $scriptroot/artifacts/publish/DreamPotato.MonoGame/DreamPotato.app/Contents/Resources/
 
+echo "Running dotnet publish..."
 dotnet publish $scriptroot/src/DreamPotato.MonoGame -c Release -r osx-x64 --self-contained
 dotnet publish $scriptroot/src/DreamPotato.MonoGame -c Release -r osx-arm64 --self-contained
 
+echo "Creating universal binaries..."
 lipo -create $scriptroot/artifacts/publish/DreamPotato.MonoGame/release_osx-arm64/DreamPotato \
     $scriptroot/artifacts/publish/DreamPotato.MonoGame/release_osx-x64/DreamPotato \
     -output $scriptroot/artifacts/publish/DreamPotato.MonoGame/DreamPotato.app/Contents/MacOS/DreamPotato
@@ -19,11 +22,13 @@ lipo -create $scriptroot/artifacts/publish/DreamPotato.MonoGame/release_osx-arm6
     $scriptroot/artifacts/publish/DreamPotato.MonoGame/release_osx-x64/libnfd.dylib \
     -output $scriptroot/artifacts/publish/DreamPotato.MonoGame/DreamPotato.app/Contents/MacOS/libnfd.dylib
 
+echo "Copying Content and Data folders..."
 cp -R $scriptroot/artifacts/bin/DreamPotato.MonoGame/release/Content/ $scriptroot/artifacts/publish/DreamPotato.MonoGame/DreamPotato.app/Contents/Resources/Content
 cp -R $scriptroot/artifacts/bin/DreamPotato.MonoGame/release/Data/ $scriptroot/artifacts/publish/DreamPotato.MonoGame/DreamPotato.app/Contents/MacOS/Data
-cp $scriptroot/src/DreamPotato.MonoGame/Info.plist $scriptroot/artifacts/publish/DreamPotato.MonoGame/DreamPotato.app/Info.plist
+cp $scriptroot/src/DreamPotato.MonoGame/Info.plist $scriptroot/artifacts/publish/DreamPotato.MonoGame/DreamPotato.app/Contents/Info.plist
 
 # Icons
+echo "Writing icons..."
 mkdir -p $scriptroot/artifacts/bin/DreamPotato.MonoGame/release/DreamPotato.iconset
 sips -z 16 16 $scriptroot/src/DreamPotato.MonoGame/Icon.png --out $scriptroot/artifacts/bin/DreamPotato.MonoGame/release/DreamPotato.iconset/icon_16x16.png
 sips -z 32 32 $scriptroot/src/DreamPotato.MonoGame/Icon.png --out $scriptroot/artifacts/bin/DreamPotato.MonoGame/release/DreamPotato.iconset/icon_16x16@2x.png
@@ -35,4 +40,5 @@ sips -z 256 256 $scriptroot/src/DreamPotato.MonoGame/Icon.png --out $scriptroot/
 sips -z 512 512 $scriptroot/src/DreamPotato.MonoGame/Icon.png --out $scriptroot/artifacts/bin/DreamPotato.MonoGame/release/DreamPotato.iconset/icon_256x256@2x.png
 sips -z 512 512 $scriptroot/src/DreamPotato.MonoGame/Icon.png --out $scriptroot/artifacts/bin/DreamPotato.MonoGame/release/DreamPotato.iconset/icon_512x512.png
 sips -z 1024 1024 $scriptroot/src/DreamPotato.MonoGame/Icon.png --out $scriptroot/artifacts/bin/DreamPotato.MonoGame/release/DreamPotato.iconset/icon_512x512@2x.png
-iconutil -c icns $scriptroot/artifacts/bin/DreamPotato.MonoGame/release/DreamPotato.iconset --output $scriptroot/artifacts/publish/DreamPotato.MonoGame/DreamPotato.app/Contents/Resources/DreamPotato.icns
+iconutil -c icns $scriptroot/artifacts/bin/DreamPotato.MonoGame/release/DreamPotato.iconset --output $scriptroot/artifacts/publish/DreamPotato.MonoGame/DreamPotato.icns
+cp $scriptroot/artifacts/publish/DreamPotato.MonoGame/DreamPotato.icns $scriptroot/artifacts/publish/DreamPotato.MonoGame/DreamPotato.app/Contents/Resources/DreamPotato.icns
