a0ae8114584c8c7c9279992b047d1fadfd561eaa
Rikki Gibson <rikkigibson@gmail.com>
2025-11-09 13:14:53 -0800

    WIP

--- src/DreamPotato.Tests/SerialTests.cs
+++ src/DreamPotato.Tests/SerialTests.cs
@@ -8,13 +8,24 @@ public class SerialTests
     [Fact]
     public void Main()
     {
-        var cpuTx = new Cpu() { DreamcastSlot = DreamcastSlot.Slot1 };
+        // not1  ext, $00
+        // jmpf  $0139
+        ReadOnlySpan<byte> stubBiosTickHandler = [
+            (OpcodeMask.NOT1 | 0b1_0000 /*d8*/ | 0b000 /*b2-0*/), SpecialFunctionRegisterIds.Ext,
+            OpcodeMask.JMPF, 0x01, 0x39,
+        ];
+
+        var cpuTx = new Cpu() { DisplayName = "SioTx", DreamcastSlot = DreamcastSlot.Slot1 };
+        cpuTx.SFRs.Btcr = new Btcr { CountEnable = true, Int0Enable = true };
+        stubBiosTickHandler.CopyTo(cpuTx.ROM.AsSpan(0x130));
         cpuTx.Reset();
 
         File.ReadAllBytes("TestSource/SioTx.vms").CopyTo(cpuTx.FlashBank0);
         cpuTx.SetInstructionBank(InstructionBank.FlashBank0);
 
-        var cpuRx = new Cpu() { DreamcastSlot = DreamcastSlot.Slot2 };
+        var cpuRx = new Cpu() { DisplayName = "SioRx", DreamcastSlot = DreamcastSlot.Slot2 };
+        cpuRx.SFRs.Btcr = new Btcr { CountEnable = true, Int0Enable = true };
+        stubBiosTickHandler.CopyTo(cpuRx.ROM.AsSpan(0x130));
         cpuRx.Reset();
 
         File.ReadAllBytes("TestSource/SioRx.vms").CopyTo(cpuRx.FlashBank0);
