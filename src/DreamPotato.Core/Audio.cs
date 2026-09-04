7d07ce0ceeec82a7145513480b550e688e983929
Rikki Gibson <rikkigibson@gmail.com>
2026-09-03 19:39:48 -0700

    WIP

--- src/DreamPotato.Core/Audio.cs
+++ src/DreamPotato.Core/Audio.cs
@@ -10,7 +10,7 @@ public class Audio
     // This won't work well with RC oscillator sounds.
     // Could consider using a separate sample rate of like 43600 there (872000 / 43600 = 20).
     // But that would really just be a way to avoid doing our own resampling
-    public const int SampleRate = OscillatorHz.Quartz;
+    public const int SampleRate = 48000;
     public const int SampleSize = 2; // 16-bit
     public const int BufferDurationMilliseconds = 8;
     private const int MaxTotalBufferDurationMilliseconds = 96;
@@ -95,11 +95,98 @@ public class Audio
     /// <summary>When CPU speed is not evenly divisible by sample rate, tracks how far we were into a single sample.</summary>
     private int _pcmRemainder;
 
+    private double _partialSignal;
+    private double _partialSample;
+
+    internal bool AddPulse(int cpuClockHz, byte t1l)
+    {
+        Debug.Assert(_partialSample is >= 0 and < 1.0);
+        Debug.Assert(Math.Abs(_partialSignal) is >= 0 and <= short.MaxValue);
+
+        // Step 1:
+        // Compute samplesPerCycle.
+        // Subtract an amount not exceeding samplesPerCycle or _sampleRemainder from _sampleRemainder.
+        // Add signal value to _signalRemainder proportional to _sampleRemainder.
+        // Assuming _sampleRemainder was reduced to zero, enqueue the sample into buffer.
+        var samplesPerCycle = (double)SampleRate / cpuClockHz;
+        var pulseValue = t1l >= _compare;
+        var sampleVolume = GetSampleVolume();
+        if (!pulseValue)
+            sampleVolume = (short)-sampleVolume;
+
+        if (_partialSample != 0)
+        {
+            // Append to partial sample.
+            var remaining = 1.0 - _partialSample;
+            var isComplete = remaining < samplesPerCycle;
+            var toRemove = isComplete ? remaining : samplesPerCycle;
+            _partialSignal += sampleVolume * (double)toRemove;
+            _partialSample += toRemove;
+            samplesPerCycle -= toRemove;
+
+            if (isComplete)
+            {
+                appendSample((short)Math.Round(_partialSignal));
+                _partialSample = 0;
+                _partialSignal = 0;
+            }
+        }
+
+        if (samplesPerCycle == 0)
+            return pulseValue;
+
+        Debug.Assert(_partialSample == 0);
+        var nSamples = (int)samplesPerCycle;
+        for (var i = 0; i < nSamples; i++)
+        {
+            appendSample(sampleVolume);
+        }
+
+        // Setup pending sample for next call.
+        _partialSample = samplesPerCycle - nSamples;
+        _partialSignal = sampleVolume * _partialSample;
+        return pulseValue;
+
+        void appendSample(short sample)
+        {
+            _pcmBuffer[_pcmBufferIndex++] = (byte)(sample & 0xff);
+            _pcmBuffer[_pcmBufferIndex++] = (byte)(sample >> 8 & 0xff);
+
+            if (_pcmBufferIndex == _pcmBuffer.Length)
+            {
+                if (!hasRealSignal())
+                {
+                    Debug.Assert(_emptyPcmBuffer.All(b => b == 0));
+                    AudioBufferReady?.Invoke(new(_emptyPcmBuffer, Start: 0, Length: _pcmBufferIndex));
+                }
+                else
+                {
+                    AudioBufferReady?.Invoke(new(_pcmBuffer, Start: 0, Length: _pcmBufferIndex));
+                }
+
+                _pcmBufferIndex = 0;
+                _pcmRemainder = 0;
+            }
+        }
+
+        // TODO2: This seems to cut off some signals in practice
+        bool hasRealSignal()
+        {
+            for (var j = 2; j < _pcmBuffer.Length; j += 2)
+            {
+                if (_pcmBuffer[j] != _pcmBuffer[0] || _pcmBuffer[j + 1] != _pcmBuffer[1])
+                    return true;
+            }
+
+            return false;
+        }
+    }
+
     /// <summary>
     /// Appends a pulse <see cref="value"/> to the PCM buffer for 1 cycle at <see cref="cpuClockHz"/>
     /// Returns the pulse value that was appended (low or high)
     /// </summary>
-    internal bool AddPulse(int cpuClockHz, byte t1l)
+    internal bool AddPulse2(int cpuClockHz, byte t1l)
     {
         var sampleRateAndRemainder = SampleRate + _pcmRemainder;
         var samplesPerCycle = sampleRateAndRemainder / cpuClockHz;
